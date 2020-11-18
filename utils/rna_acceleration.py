2818e5452eb02bf1084da97ca9d5f673bdbfe267
Massimiliano Lupo Pasini <massimiliano.lupo.pasini@gmail.com>
2020-11-18 15:48:58 -0500

    fixed call to least squares solve
    Former-commit-id: 7f57c3f768043734526dd027a20e531a80e8e00f
    Former-commit-id: cbb432134f14a8dfce7fa7105f7a6c53358f17c4

--- utils/rna_acceleration.py
+++ utils/rna_acceleration.py
@@ -80,8 +80,7 @@ def rna_precomputed(X, RR, reg=0):
     try:
         z = np.linalg.solve(RR + reg_I, np.ones(k))
     except LA.linalg.LinAlgError:
-        z = np.linalg.lstsq(RR + reg_I, np.ones(k), -1)
-        z = z[0]
+        z, = np.linalg.lstsq(RR + reg_I, np.ones(k), -1)
 
     # Recover weights c, where sum(c) = 1
     if np.abs(np.sum(z)) < 1e-10:
