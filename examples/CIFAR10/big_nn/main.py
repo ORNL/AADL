c29af18f63784b713c118745c7544c8f324d185f
Massimiliano Lupo Pasini <lupopasinim@ornl.gov>
2021-03-28 18:41:59 -0400

    added author list
    Former-commit-id: 16b138198831308347bdb3ec2125d15657f4c695
    Former-commit-id: 5b214a0186b52271d80e55d2b958799c5d3bef05

--- examples/CIFAR10/big_nn/main.py
+++ examples/CIFAR10/big_nn/main.py
@@ -1,4 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@authors: Massimiliano Lupo Pasini (e-mail: lupopasinim@ornl.gov.gov)
+        : Miroslav Stoyanov (e-mail: stoyanovmk@ornl.gov.gov)
+        : Viktor Reshniak (e-mail: reshniakv@ornl.gov.gov)
+"""
 '''Train CIFAR10 with PyTorch.'''
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
