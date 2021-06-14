5b56bfb14b338f10305f5522836503c151fccc32
Massimiliano Lupo Pasini <massimiliano.lupo.pasini@gmail.com>
2021-06-14 11:05:19 -0400

    non convex problem updated
    Former-commit-id: 6dd2463ee3161ae681b010d15f575a48ffeeb26e
    Former-commit-id: 2266e142afa9ba351bc5a85c43cf13f14d12324d

--- tests/test_linear_regression_nonconvex_loss_function.py
+++ tests/test_linear_regression_nonconvex_loss_function.py
@@ -75,10 +75,10 @@ def linear_data(slope, intercept, num_points: int = 10):
 def linear_regression(slope, intercept, num_points, optimizer_str):
     input_dim, output_dim, dataset = linear_data(slope, intercept, num_points)
     use_bias = True
-    learning_rate = 1e-4
+    learning_rate = 1e-5
     weight_decay = 0.0
     batch_size = 1
-    epochs = 1000
+    epochs = 100
     threshold = 1e-8
 
     training_dataloader = torch.utils.data.DataLoader(dataset, batch_size)
@@ -95,30 +95,65 @@ def linear_regression(slope, intercept, num_points, optimizer_str):
 
     return weights[0].item(), weights[1].item(), validation_classic_loss_history
 
-
-def linear_regression_anderson(slope, intercept, num_points, optimizer_str):
+def linear_regression_average(slope, intercept, num_points, optimizer_str):
     input_dim, output_dim, dataset = linear_data(slope, intercept, num_points)
     acceleration_type = "identity"
     use_bias = True
-    learning_rate = 1e-4
+    learning_rate = 1e-5
     relaxation = 1.0
     weight_decay = 0.0
     batch_size = 1
-    epochs = 1000
+    epochs = 100
     threshold = 1e-8
     wait_iterations = 1
-    history_depth = 10
-    frequency = 5   
-    reg_acc = 1e-9
+    history_depth = 3
+    frequency = 3
+    reg_acc = 1e-8
     store_each_nth = frequency
     average = True
+    safeguard = True
 
     training_dataloader = torch.utils.data.DataLoader(dataset, batch_size)
     validation_dataloader = torch.utils.data.DataLoader(dataset, batch_size)
 
     model = LinearRegression(input_dim, output_dim, use_bias)
     optimizer_anderson = DeterministicAcceleration(training_dataloader,validation_dataloader,acceleration_type,learning_rate,relaxation,weight_decay,wait_iterations,history_depth,
-        frequency,reg_acc,store_each_nth, average)
+        frequency,reg_acc,store_each_nth, average, safeguard)
+    optimizer_anderson.import_model(model)
+    optimizer_anderson.set_loss_function('nonconvex')
+    optimizer_anderson.set_optimizer(optimizer_str)
+    training_anderson_loss_history, validation_anderson_loss_history, _ = optimizer_anderson.train(epochs, threshold, batch_size)
+
+    weights = list(model.get_model().parameters())
+
+    return weights[0].item(), weights[1].item(), validation_anderson_loss_history
+
+
+def linear_regression_anderson(slope, intercept, num_points, optimizer_str):
+    input_dim, output_dim, dataset = linear_data(slope, intercept, num_points)
+    acceleration_type = "anderson"
+    use_bias = True
+    learning_rate = 1e-5
+    relaxation = 1.0
+    weight_decay = 0.0
+    batch_size = 1
+    epochs = 100
+    threshold = 1e-8
+    wait_iterations = 1
+    history_depth = 5
+    frequency = 3
+    reg_acc = 1e-8
+    store_each_nth = frequency
+    average = True
+    safeguard = True
+
+    training_dataloader = torch.utils.data.DataLoader(dataset, batch_size)
+    validation_dataloader = torch.utils.data.DataLoader(dataset, batch_size)
+
+    model = LinearRegression(input_dim, output_dim, use_bias)
+    optimizer_anderson = DeterministicAcceleration(training_dataloader,validation_dataloader,acceleration_type,learning_rate,relaxation,weight_decay,wait_iterations,history_depth,
+        frequency,reg_acc,store_each_nth, average, safeguard)
+    
     optimizer_anderson.import_model(model)
     optimizer_anderson.set_loss_function('nonconvex')
     optimizer_anderson.set_optimizer(optimizer_str)
@@ -130,7 +165,7 @@ def linear_regression_anderson(slope, intercept, num_points, optimizer_str):
 
 
 def test_linear_regression(optimiser):
-    num_points = 200
+    num_points = 3000
     straight_line_parameters = torch.ones(2, 1)
     slope = straight_line_parameters[0].item()
     intercept = straight_line_parameters[1].item()
@@ -138,10 +173,22 @@ def test_linear_regression(optimiser):
     print(optimiser+" converged in "+ str(len(history))+" iterations "+"\n exact slope: "+ str(slope)+"  - "+ " numerical slope: "+str(numeric_slope)+"\n"+" exact intercept: "
         + str(intercept)+" - "+" numerical intercept: "+str(numeric_intercept))
     assert(abs((slope - numeric_slope)) < 1e-3 and abs((intercept - numeric_intercept)) < 1e-3)
+    
+def test_linear_regression_average(optimiser):
+    num_points = 3000
+    straight_line_parameters = torch.ones(2, 1)
+    slope = straight_line_parameters[0].item()
+    intercept = straight_line_parameters[1].item()
+    numeric_slope, numeric_intercept, history = linear_regression_average(
+        slope, intercept, num_points, optimiser
+    )
+    print(optimiser+" + Average converged in "+ str(len(history))+" iterations "+"\n exact slope: "+ str(slope)+"  - "+ " numerical slope: "+str(numeric_slope)+"\n"+" exact intercept: "
+        + str(intercept)+" - "+" numerical intercept: "+str(numeric_intercept))
+    assert(abs((slope - numeric_slope)) < 1e-3 and abs((intercept - numeric_intercept)) < 1e-3)    
 
 
 def test_linear_regression_anderson(optimiser):
-    num_points = 200
+    num_points = 3000
     straight_line_parameters = torch.ones(2, 1)
     slope = straight_line_parameters[0].item()
     intercept = straight_line_parameters[1].item()
@@ -168,26 +215,28 @@ class TestLinearRegression(unittest.TestCase):
     def test_rmsprop(self):
         test_linear_regression('rmsprop')
         
-    """
 
     def test_adam(self):
         test_linear_regression('adam')
-   
+    """
+    def test_sgd_average(self):
+        test_linear_regression_average('sgd')
 
     def test_sgd_anderson(self):
         test_linear_regression_anderson('sgd')
         
     """
+
     def test_asgd_anderson(self):
         test_linear_regression_anderson('asgd')
 
     def test_rmsprop_anderson(self):
         test_linear_regression_anderson('rmsprop')
     
-    """
 
     def test_adam_anderson(self):
         test_linear_regression_anderson('adam')
+    """
     
 
 ###############################################################################
