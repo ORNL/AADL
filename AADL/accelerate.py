found bug in how I was computing the variance
Former-commit-id: 1b9c30256b6b04eb741a4f76eb4e9030523574d8
Former-commit-id: a167134b778ed2fd3f70dd24b156aa7b19c09e49

AADL/accelerate.py
@@ -57,9 +57,9 @@ def averaged_step(self, closure=None):
     for group, group_hist in zip(self.param_groups, self.avg_param_hist):
         X = torch.stack(list(group_hist), dim=1)
         average = torch.mean(X, dim=1)
-        std = torch.stack(list(group_hist), dim=1)
+        std = torch.std(X, dim=1)
             
-        if torch.max(std)/torch.max(average)>1.0:
+        if torch.max(std)/torch.max(average)>0.1:
             # load acceleration back into model and update history
             vector_to_parameters(average, group['params'])
      
@@ -74,11 +74,11 @@ def averaged_accelerated_step(self, closure=None):
     for group, group_hist in zip(self.param_groups, self.avg_param_hist):
         X = torch.stack(list(group_hist), dim=1)
         average = torch.mean(X, dim=1)
-        std = torch.stack(list(group_hist), dim=1)
+        std = torch.std(X, dim=1)
             
         #print(torch.norm(std), torch.norm(average), torch.norm(std)/torch.norm(average))
             
-        if torch.max(std)/torch.max(average)>1.0:
+        if torch.max(std)/torch.max(average)>0.1:
             # load acceleration back into model and update history
             vector_to_parameters(average, group['params'])
                 